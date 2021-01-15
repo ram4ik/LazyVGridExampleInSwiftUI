9cb55469d38a6d33d6afcea0b9759b02d8696b48
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-01-15 10:04:58 +0200

    Initial Commit

---
+++ LazyVGridExampleInSwiftUI/LazyVGridExampleInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVGridExampleInSwiftUIApp.swift
+//  LazyVGridExampleInSwiftUI
+//
+//  Created by Ramill Ibragimov on 15.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct LazyVGridExampleInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
